f23231b6ea8e7730f98e55fd43c5987e67bfe82a
Matthew Swan <swanm86@gmail.com>
2016-04-20 10:13:59 -0500

    assessment for code platoon

--- lib/chip.rb
+++ lib/chip.rb
@@ -1,1 +1,47 @@
 #write code here to make some tests pass
+class Chip
+  attr_reader :color, :amt
+  def initialize(color, amt)
+    @color = color
+    @amt = amt
+    @face_up = true
+  end
+
+  def value
+    @amt
+  end
+
+  def face_up?
+    @face_up
+  end
+
+  def flip
+    if @face_up == true
+      @face_up = false
+    else
+      @face_up = true
+    end
+  end
+
+  include Comparable
+  def >(chip)
+    if @amt > chip.value
+      return true
+    end
+    return false
+  end
+
+  def <(chip)
+    if @amt < chip.value
+      return true
+    end
+    return false
+  end
+
+  def ==(chip)
+    if @amt == chip.value
+      return true
+    end
+    return false
+  end
+end
--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,27 @@
 #write code here to make some tests pass
+class Stack
+  attr_reader :chips
+  def initialize()
+    @chips =  {black: [],
+               green: [],
+               red: [],
+               white: [],
+              }
+  end
+
+  def add_chips(seed_chips)
+    seed_chips.each do |chip|
+      chips[chip.color]  << chip
+    end
+  end
+
+  def count
+    sum = 0
+    @chips.each do |key, value|
+      value.each do |amount|
+        sum += amount.amt
+      end
+    end
+    sum
+  end
+end
